81636e9ef77186e966f712ae1b00eca015a9bed5
Khakachang <khakachang22@gmail.com>
2024-08-18 00:54:45 +0530

    GG

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main(){
+    let mut x : u8 = 32;
+    println!("The value of x is {}", x);
+
+    x = 69;
+    println!("The value of x is now {}", x);
+
+    let arr : [u8; 4] = [12, 3, 42, 11];
+    let mut arr_size : usize = arr.len();
+
+    while arr_size != 0 {
+        println!("Array element {} value is {}", arr_size - 1, arr[arr_size-1]);
+        arr_size -= 1;
+    }  
+    some_function();
+}
+
+fn some_function(){
+    println!("Some Function is called");
+}
